b90267243019c2449b3734c63d2ca205805953c7
utkususoy <utkususoy@gmail.com>
2022-01-25 00:29:49 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from distutils.log import debug
+from unittest import result
+from flask import Flask, jsonify, request, render_template
+import sys
+sys.path.append('../Models')
+import Svm_Prediction
+
+app = Flask(__name__)
+
+res = ""
+
+@app.route("/")
+def welcome_page():
+    return render_template("index.html")
+
+@app.route("/model", methods=['POST'])
+def receive_text():
+    try:
+        global res
+#    request_data = request.get_json()
+        request_data = request.form["pred_text"]
+
+    # modeli çağırıp predict ettir.
+        result = Svm_Prediction.get_predict(request_data)
+        print("received {}".format(result))
+        return render_template('index.html', res = result[0])
+    except Exception as e:
+        return "{}".format(e)
+app.run(port=5000, debug=True)
